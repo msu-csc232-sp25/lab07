44e02720e089e365e431e69f620b3361c44f3594
Professor Daehn <jdaehn@missouristate.edu>
2025-03-31 15:34:07 -0500

    Initial import of lab

--- src/include/magic_box.h
+++ src/include/magic_box.h
@@ -10,17 +10,6 @@
 namespace csc232
 {
     // Task 1a - Declare the MagicBox class below
-    template <typename T>
-    class MagicBox : public PlainBox<T>
-    {
-    private:
-        bool first_item_stored;
-
-    public:
-        MagicBox( );
-        explicit MagicBox( const T &an_item );
-        void set_item( const T &an_item ) override;
-    };
 
 }
 
